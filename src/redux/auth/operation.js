f7e774c2deff371ae9daabac58f967bd6bfaf46f
Dmitry Natalchuk <next1996@ukr.net>
2022-10-31 15:42:46 +0200

    start task operation auth

---
+++ src/redux/auth/operation.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const setAuthToken = token => {
+  if (token) {
+    return (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
+  }
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/signup', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/login', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
